45f828e55cca1f497fc0f1d40926ee29597ebd45
DESKTOP-3DA3S16\dgw <jainee.wang@digiwin.com>
2024-09-19 16:40:42 +0800

    Increase 精選文章PC

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+// import styles bundle
+import 'swiper/css/bundle';
+
+const swiper = new Swiper(".mySwiper", {
+  //一次呈現幾張
+  slidesPerView: 2,
+  //一個群組內有幾張投影片
+  slidesPerGroup: 2,
+  spaceBetween: 48,
+
+  pagination: {
+    el: ".swiper-pagination-num",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+});
